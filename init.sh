9b09dee564dab908a87492de4ef0c243b87f37a1
Dima Kruk <krkd@gmail.com>
2017-02-16 11:21:14 +0200

    Added install of redis-server to init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 apt-get update
-apt-get install -y python-dev python-pip libssl-dev
+apt-get install -y python-dev python-pip libssl-dev redis-server
 pip install gevent redis https://github.com/unbit/uwsgi/archive/master.zip
